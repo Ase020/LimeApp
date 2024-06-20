15cd81279145422f5e5cd77eee82d30b98f1a995
felixNyalenda <felixasejunior@gmail.com>
2024-06-20 11:46:09 +0300

    Clustering

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,6 @@
 import Mapbox, {
   Camera,
+  CircleLayer,
   Images,
   LocationPuck,
   MapView,
@@ -21,13 +22,43 @@ export default function Map() {
       <Camera followUserLocation followZoomLevel={12} />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
 
-      <ShapeSource id="scooters" shape={featureCollection(points)}>
+      <ShapeSource
+        id="scooters"
+        shape={featureCollection(points)}
+        cluster
+        onPress={(e) => console.log(JSON.stringify(e, null, 2))}>
+        <SymbolLayer
+          id="clusters-count"
+          style={{
+            textField: ['get', 'point_count'],
+            textSize: 16,
+            textColor: '#ffffff',
+            textPitchAlignment: 'map',
+          }}
+        />
+
+        <CircleLayer
+          id="clusters"
+          belowLayerID="clusters-count"
+          filter={['has', 'point_count']}
+          style={{
+            circlePitchAlignment: 'map',
+            circleColor: '#42E100',
+            circleRadius: 20,
+            circleOpacity: 0.6,
+            circleStrokeWidth: 2,
+            circleStrokeColor: 'white',
+          }}
+        />
+
         <SymbolLayer
           id="scooter-icons"
+          filter={['!', ['has', 'point_count']]}
           style={{
             iconImage: 'pin',
             iconSize: 0.5,
             iconAllowOverlap: true,
+            iconAnchor: 'bottom',
           }}
         />
 
